a20fc59bce82a066ebdc36fed1aec19263d9b74f
Melania Aceto <melaniaaceto@gmail.com>
2021-10-15 10:59:42 +0200

    new exercise

---
+++ src/main/java/m02/s07/Ciao.java
@@ -0,0 +1,16 @@
+package m02.s07;
+
+public class Ciao {
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			System.out.println("Un argo");
+
+		} else if (args.length == 2) {
+			System.out.println("Due argo");
+
+		} else {
+			System.out.println("Ciao");
+		}
+
+	}
+}
--- src/main/java/m02/s10/ex/Exercise1.java
+++ src/main/java/m02/s10/ex/Exercise1.java
@@ -1,10 +1,26 @@
 package m02.s10.ex;
 
 public class Exercise1 {
-    public static void main(String[] args) {
-        // TODO: print all values in [0 .. 9]
-        // (1) for loop
-        // (2) while loop
-        // (3) do-while loop
-    }
+	public static void main(String[] args) {
+		System.out.println("print all values in [0 .. 9]");
+
+		// TODO: print all values in [0 .. 9]
+		// (1) for loop
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		// (2) while loop
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i);
+			i = i + 1;
+		}
+		// (3) do-while loop
+		int i1 = 0;
+		do {
+			System.out.println(i1);
+			i1 = i1 + 1;
+		} while (i1 < 10);
+
+	}
 }
